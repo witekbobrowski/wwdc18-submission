acb0a06914775a66daea93074a379cc2626f1040
Witek Bobrowski <witek@bobrowski.com.pl>
2018-03-26 01:15:45 +0200

    Remove old playground file.

--- submission.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++
@@ -1,5 +0,0 @@
-//: Playground - noun: a place where people can play
-import PlaygroundSupport
-
-let menuViewController = MenuViewController()
-PlaygroundPage.current.liveView = menuViewController.view
