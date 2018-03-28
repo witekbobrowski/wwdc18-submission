40f686ea861b57b6f6ac7c88dde4968541c61321
Witek Bobrowski <witek@bobrowski.com.pl>
2018-03-28 23:48:13 +0200

    Add LibraryService.

---
+++ iPod/iPod/OS/Services/LibraryService.swift
@@ -0,0 +1,30 @@
+//
+//  LibraryService.swift
+//  iPod
+//
+//  Created by Witek Bobrowski on 28/03/2018.
+//  Copyright © 2018 Witek Bobrowski. All rights reserved.
+//
+
+import Foundation
+
+protocol LibraryService {
+    var songs: [Song] { get }
+    var artists: [Artist] { get }
+    var playlists: [Playlist] { get }
+    var favourites: [Song] { get }
+    func albumsOfArtist(_ artist: Artist) -> [Album]
+}
+
+class LibraryServiceImplementation: LibraryService {
+
+    var songs: [Song] { return [] }
+    var artists: [Artist] { return [] }
+    var playlists: [Playlist] { return [] }
+    var favourites: [Song] { return [] }
+
+    func albumsOfArtist(_ artist: Artist) -> [Album] {
+        return []
+    }
+
+}
